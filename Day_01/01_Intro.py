7a4edb9d5256e357876a223836490b7e15b739a0
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-11-28 22:47:30 +0530

    Day 01

---
+++ Day_01/01_Intro.py
@@ -0,0 +1,18 @@
+# 28/11/2022 # Monday
+
+# Introduction to Programming & Python | Python Tutorial - Day #1
+
+# https://www.youtube.com/watch?v=7wnove7K-ZQ
+
+# Python is one of the most demanded programming languages in the job market. Surprisingly, it is equally easy to learn and master  Python. This python tutorial for absolute beginners in Hindi series will focus on teaching you python concepts from the ground up.
+
+# Download Python => python.org
+
+# IDE => VS code 
+
+# https://replit.com/@codewithharry/01-Day1-Intro-to-Python#main.py
+
+
+print("100 Days of Code")
+
+print(100)
