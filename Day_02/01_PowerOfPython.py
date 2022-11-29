504194a9d46d70c4c4e5c1c07af10149cfbc4f85
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-11-29 19:54:54 +0530

    Some Amazing Python Programs - The Power of Python

---
+++ Day_02/01_PowerOfPython.py
@@ -0,0 +1,15 @@
+# Some Amazing Python Programs - The Power of Python | Python Tutorial - Day #2
+
+# https://www.youtube.com/watch?v=Tto8TS-fJQU
+
+# Top Python Projects
+
+# 1) Jarvis Virtual Assistant
+
+# 2) Love Calculator
+
+# 3) Face Recognition
+
+# 4) Flappy Bird
+
+# 5) Snake Game
