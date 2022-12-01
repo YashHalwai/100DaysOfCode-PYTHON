531983b9be0d25c638458fbe665fff7f4dc46af1
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-01 23:31:19 +0530

    First Python Program

---
+++ Day_04/01_firstProgram.py
@@ -0,0 +1,18 @@
+# Our First Python Program | Python Tutorial - Day #4
+
+# https://www.youtube.com/watch?v=7IWOYhfAcVg&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=4
+
+print("Hello World") #function
+
+print(5)
+
+print("Bye")
+
+print(17 * 13) # (+  -  /  %  *  //  **)
+
+
+# Write a program to print a poem in Python
+
+print(
+    "The forest is the town of trees Where they live quite at their ease, With their neighbors at their side Just as we in cities wide."
+)
