67face8fa65bc57d36d3c5dfd1b8bc5536babd7b
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-01 23:18:26 +0530

    Modules & Pip

---
+++ Day_03/01_modulesPip.py
@@ -0,0 +1,15 @@
+# Modules and Pip | Python Tutorial - Day #3
+
+# https://www.youtube.com/watch?v=xwKO_y2gHxQ&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=3
+
+# pip install pandas
+
+import pandas
+
+print("Hi")
+
+# built in modules
+import hashlib
+
+# external modules
+import tensorflow # pip install tensorflow
