ba0b021efb40796011cf751f088916365aa80bf0
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-02 23:50:23 +0530

    Comments, Escape Sequences & Print Statement

---
+++ Day_05/01_Lec5.py
@@ -0,0 +1,30 @@
+# Comments, Escape Sequences & Print Statement | Python Tutorial - Day #5
+
+# https://www.youtube.com/watch?v=qxPMmW93eDs
+
+print("Hey, I am a good boy \nand this viewer is also a good \n boy/girl")
+
+# \n => Escape Sequence Character
+
+# Comment
+
+# Author: Harry Bhai
+# Course: 100 Days of Code - PYTHON
+
+# print("Hello World")
+
+'''
+Multi - Line
+Comment
+'''
+
+"""
+Multi -Line
+Comment
+"""
+
+print("Hey, I am a \"good boy\" \nand this viewer is also a good \n boy/girl")
+
+print("Hey", 6, 7, sep="~")
+
+print("Harry")
