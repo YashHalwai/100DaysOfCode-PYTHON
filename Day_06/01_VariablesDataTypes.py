78049a6d90d72dace766b3810a7f07e34706cd6e
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-04 23:09:00 +0530

    Variables and Data Types

---
+++ Day_06/01_VariablesDataTypes.py
@@ -0,0 +1,36 @@
+# Variables and Data Types | Python Tutorial - Day #6
+
+# https://www.youtube.com/watch?v=ORCuz7s5cCY&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=6
+
+a = 1543465454
+
+print(a)
+
+b = "Harry"
+
+print(b)
+
+
+c = 45
+d = 7
+
+print (c + d)
+
+print("The type of c is ", type(c))
+
+e = 1.1
+print("The type of e is ", type(e))
+
+f = complex(8, 2)
+print("The type of f is ", type(f))
+
+list1 = [8, 2.3, [4,-5], ["apple", "banana"]] #mutable
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger")) #immutable
+print(tuple1)
+
+dict1 = {"name1":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+
+# In Python all are object
