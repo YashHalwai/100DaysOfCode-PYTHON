e6ed6216999d54a35697e6c6731240f92a6485a7
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-06 00:05:10 +0530

    Calculator using Python (Solution)

---
+++ Day_08/Exercise1_soln.py
@@ -0,0 +1,11 @@
+# Exercise 1: Calculator using Python (Solution) | Python Tutorial - Day #8
+
+# https://www.youtube.com/watch?v=dohaSBCKCr0
+
+a = 50
+b = 3
+
+print("The value of", a, " + ", b, "is ", a + b)
+print("The value of", a, " - ", b, "is ", a - b)
+print("The value of", a, " * ", b, "is ", a * b)
+print("The value of", a, " / ", b, "is ", a / b)
