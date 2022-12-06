9c328876b5abb48c04e3bf9e8d38ab323b94140d
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-06 23:44:46 +0530

    Typecasting in Python

---
+++ Day_09/Typecasting.py
@@ -0,0 +1,23 @@
+# Typecasting in Python | Python Tutorial - Day #9
+
+# https://www.youtube.com/watch?v=Pu5bqySSSS0
+
+a = "1"
+b = "2"
+
+print(a + b)
+
+a = 1
+b = 2
+print(a + b)
+
+c = "10"
+d = "10"
+print(int(c) + int(d))
+
+# Explicit Typecasting => done by developer or programmer
+
+# Implicit Typecasting => python interpreter does it automatically
+e = 1.9
+f = 5
+print(e + f) # implicit
