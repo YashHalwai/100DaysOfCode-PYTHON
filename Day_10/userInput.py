00ec8305aeb64eb215e5860ec2cb4bd016596dd5
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-08 23:27:41 +0530

    Taking User Input in Python

---
+++ Day_10/userInput.py
@@ -0,0 +1,15 @@
+# Taking User Input in Python | Python Tutorial - Day #10
+
+# https://www.youtube.com/watch?v=WvG-R-xXouA
+
+# input => FUNCTION
+
+# a = input()
+# print("My name is", a)
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+
+print("Sum of two number is", x+y)
+
+# print(int(x) + int(y))
