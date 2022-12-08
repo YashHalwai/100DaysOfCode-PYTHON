850214b1fe4c1a1ae2d810ae5050ec2f2796b2c1
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-08 23:44:24 +0530

    Strings in Python

---
+++ Day_11/strings.py
@@ -0,0 +1,35 @@
+# Strings in Python | Python Tutorial - Day #11
+
+# https://www.youtube.com/watch?v=kMNFQYArrLg
+
+name = "Yash"
+friend = "Sanket"
+anotherFriend = 'Atharv'
+
+# print("Hello,"+ name)
+
+# print("He said, \"I want to eat an Apple\"")
+# print('He said, "I want to eat an Apple"')
+
+# msg = '''
+
+# Hello
+# Hi
+
+# How r u
+
+# I am :)
+
+# '''
+
+# print(msg)
+
+# print(name[0])
+# print(friend[2])
+# print(anotherFriend[4])
+
+# print(name[4]) => string index out of range
+
+print("Let's use a for loop\n")
+for character in name:
+    print(character)
