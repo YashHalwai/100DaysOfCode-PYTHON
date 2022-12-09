aa46ffa17cfb7e243d9025f0c6b286ede2bfbdf7
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-10 00:38:12 +0530

    Strings Slicing and Operations on Strings in Python

---
+++ Day_12/strings.py
@@ -0,0 +1,36 @@
+# Strings Slicing and Operations on Strings in Python | Python Tutorial - Day #12
+
+# https://www.youtube.com/watch?v=8jW7lpT8HW8
+
+# names = "Harry, Shubham"
+
+# print(len(names))
+
+# print(names[0:5])
+
+# fruit = "Mango"
+
+# len1 = len(fruit)
+
+# print("Mango is a", len1, "letter word.")
+
+fruit = "Mango"
+
+mangoLen = len(fruit)
+# print(mangoLen)
+
+# print(fruit[0:4])
+# print(fruit[1:4])
+# print(fruit[:4])
+# print(fruit[:5])
+
+# print(fruit[0:-3])
+# print(fruit[0:len(fruit)-3])
+
+# print(fruit[-1:-3] 4:2 
+# print(fruit[-3:-1]) #2:4
+
+# Quick Quiz
+
+nm = "Harry"
+print(nm[-4:-2])
