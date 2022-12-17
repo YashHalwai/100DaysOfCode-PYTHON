21364b2e29108a25f3c8c221ef83039726ca734d
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-12-17 23:55:53 +0530

    While Loops in Python

---
+++ Day_18/whileLoops.py
@@ -0,0 +1,34 @@
+# While Loops in Python | Python Tutorial - Day #18
+
+# https://www.youtube.com/watch?v=-tCFyIyKVx0
+
+
+# i = int(input("Enter the number: "))
+# while(i <= 38):
+#     i = int(input("Enter the number: "))
+#     print(i)
+# print("Done with the loop")        
+
+
+# count = 5
+# while(count > 0):
+#     print(count)
+#     count = count - 1
+
+
+# count = 5
+# while(count > 0):
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside else")    
+
+
+# do while loop not work in python => execute atleast 1 time 
+'''
+
+do{
+    # loop body
+}while(condition);
+
+'''
