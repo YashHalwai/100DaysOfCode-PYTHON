1d4d38eba3355d30ea52ce429030ec25a864cd91
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-05 12:34:05 +0530

    Tuples in Python

---
+++ Day_24/tuples.py
@@ -0,0 +1,30 @@
+# Tuples in Python | Python Tutorial - Day #24
+
+# https://www.youtube.com/watch?v=PipsOUDKrVk&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=24
+
+# Tuple can not change
+
+
+# tup = (1, 5, 9)
+# print(type(tup), tup)
+
+
+# tup = (1)
+# print(type(tup), tup)
+
+
+# tup = (1,)
+# print(type(tup), tup)
+
+
+# tup = (1,5,9,8,True,"Hi")
+# print(tup[0])
+# print(tup[3])
+# print(tup[-5])
+
+# if 8 in tup:
+#     print("Yes 8 is present in this tuple.")
+
+
+# tup2 = tup[1:4]
+# print(tup2)
