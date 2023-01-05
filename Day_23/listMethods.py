04530fd3d871e8a14d5440e97cb5cdef33eb4f23
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-05 12:14:54 +0530

    List Methods in Python

---
+++ Day_23/listMethods.py
@@ -0,0 +1,48 @@
+# List Methods in Python | Python Tutorial - Day #23
+
+# https://www.youtube.com/watch?v=scWc3F8LbOo&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=23
+
+l = [1, 2, 4, 6, 2, 4, 6, 8, 7, 3, 0, 4, 5]
+# print(l)
+
+
+# l.append(7)
+# print(l)
+
+
+# l.sort(reverse=True)
+# print(l)
+
+
+# l.reverse()
+# print(l)
+
+
+# print(l.index(4))
+
+
+# print(l.count(2))
+
+
+# m = l
+# m[0] = 0
+# print(l)
+
+
+# m = l.copy()
+# m[0] = m
+# print(l)
+
+
+# l.insert(1, 90)
+# print(l)
+
+
+# m = [100, 200, 300]
+# l.extend(m)
+# print(l)
+
+
+# m = [100, 200, 300]
+# k = l + m
+# print(k)
