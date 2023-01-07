a7ebee43d9867ec3cb54ca47e40298c8304c1223
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-07 15:16:34 +0530

    Tuples in Python

---
+++ Day_25/opTuple.py
@@ -0,0 +1,28 @@
+# Operations on Tuples in Python | Python Tutorial - Day #25
+
+# https://www.youtube.com/watch?v=XblLSduioJI&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=25
+
+
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# temp = list(countries)
+# temp.append("Russia") # add item
+# temp.pop(3) # remove item
+# temp[2] = "Finland" # change item
+# countries = tuple(temp)
+# print(countries)
+
+
+# countries = ("Pakistan", "Afghanistan", "Bangladesh", "Srilanka")
+# countries2 = ("Denmark", "India", "China")
+# southEastAsia = countries + countries2
+# print(southEastAsia)
+
+
+# tuple1 = (1, 5, 6, 7, 8, 1, 2, 5, 6)
+# cnt = tuple1.count(1)
+# print("Count of 1 in tuple1 is", cnt)
+
+
+# tuple1 = (1, 5, 6, 7, 8, 1, 2, 5, 6)
+# indx = tuple1.index(1)
+# print("Index of 1 in tuple1 is", indx)
