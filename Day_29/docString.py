3335404a6a45debad498d4da2973d333418a8695
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-20 16:08:44 +0530

    Docstrings in Python

---
+++ Day_29/docString.py
@@ -0,0 +1,9 @@
+# Docstrings in Python | Python Tutorial - Day #29
+
+# https://www.youtube.com/watch?v=SJzsNd7SM0g&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=29
+
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)    
+print(square.__doc__)
