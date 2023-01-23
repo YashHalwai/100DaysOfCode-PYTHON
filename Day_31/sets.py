2fdfb85906fb13e2012026d6f96d4bc2dea03c88
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-23 19:56:42 +0530

    Sets in Python

---
+++ Day_31/sets.py
@@ -0,0 +1,23 @@
+# Sets in Python | Python Tutorial - Day #31
+
+# https://www.youtube.com/watch?v=l3kCO8cVA6o&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=31
+
+# SET is a collection of well defined objects.
+
+s = {2, 4, 2, 6}
+print(s)
+
+
+# Sets are unordered collection of data items.
+
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+
+harry = set()
+print(type(harry))
+
+
+for value in info:
+    print(value)
