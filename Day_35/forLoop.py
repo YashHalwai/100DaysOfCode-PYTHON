4f65e32a892cb40bb8beef6d7c8c1bd8e94c3693
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-24 00:07:49 +0530

    for Loop with else in Python

---
+++ Day_35/forLoop.py
@@ -0,0 +1,32 @@
+# for Loop with else in Python | Python Tutorial - Day #35
+
+# https://www.youtube.com/watch?v=qUkcIgErZzc&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=35
+
+# for i in range(5):
+#     print(i)
+
+
+# for i in []:
+#     print(i)
+# else:
+#     print("Sorry no i")    
+
+
+# i = 0
+# while i < 7:
+#     print(i)
+#     i = i + 1
+#     if i == 4:
+#         break    
+# else:
+#     print("Sorry no i")    
+ 
+ 
+# i = 0
+# while i < 7:
+#     print(i)
+#     i = i + 1
+#     # if i == 4:
+#     #     break    
+# else:
+#     print("Sorry no i")      
