a4a23c65b2037e90a498f2565c8853a41c390dc1
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-23 23:31:25 +0530

    Dictionary Methods in Python

---
+++ Day_34/dictMethods.py
@@ -0,0 +1,26 @@
+# Dictionary Methods in Python | Python Tutorial - Day #34
+
+# https://www.youtube.com/watch?v=LmbFwaLjT9k&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=34
+
+ep1 = {122:45, 123:89, 567:69, 670:69}
+ep2 = {222:67, 566:90}
+
+
+# ep1.update(ep2)
+# print(ep1)
+
+
+# ep1.clear()
+# print(ep1)
+
+
+# ep1.pop(122)
+# print(ep1)
+
+
+# ep1.popitem()
+# print(ep1)
+
+
+# del ep1
+# print(ep1)
