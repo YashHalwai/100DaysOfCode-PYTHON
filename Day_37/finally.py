0042df32d6e6f729296e0dbaaaeeb2e235d85535
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-24 18:43:03 +0530

    Finally keyword in Python

---
+++ Day_37/finally.py
@@ -0,0 +1,60 @@
+# Finally keyword in Python | Python Tutorial - Day #37
+
+# https://www.youtube.com/watch?v=r_iuC-IDpPM&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=37
+
+# Finally Clause
+
+# try:
+#     l = [1,5,6,7]
+#     i = int(input("Enter the index: "))
+#     print(l[i])
+# except:
+#     print("Some error occurred")    
+
+# finally:
+#     print("I am always executed")
+
+
+
+# try:
+#     l = [1,5,6,7]
+#     i = int(input("Enter the index: "))
+#     print(l[i])
+# except:
+#     print("Some error occurred")    
+
+# print("I am always executed")
+
+
+# def func1():
+#     try:
+#         l = [1,5,6,7]
+#         i = int(input("Enter the index: "))
+#         print(l[i])
+#         return 1
+#     except:
+#         print("Some error occurred")   
+#         return 0 
+
+#     print("I am always executed")
+        
+# x = func1()        
+# print(x)
+
+
+
+# def func1():
+#     try:
+#         l = [1,5,6,7]
+#         i = int(input("Enter the index: "))
+#         print(l[i])
+#         return 1
+#     except:
+#         print("Some error occurred")   
+#         return 0 
+
+#     finally:
+#         print("I am always executed")
+        
+# x = func1()        
+# print(x)
