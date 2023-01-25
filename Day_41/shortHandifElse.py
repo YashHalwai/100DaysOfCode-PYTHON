19d1b7836baa5c4d9617c67e07cb136c78eef764
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-25 12:48:07 +0530

    Short hand if else statements

---
+++ Day_41/shortHandifElse.py
@@ -0,0 +1,11 @@
+# Short hand if else statements | Python Tutorial - Day #41
+
+# https://www.youtube.com/watch?v=Qqx_zNmQVGI&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=41
+
+a = 330
+b = 3303
+
+print("A") if a > b else print("=") if a == b else print("B")
+
+c = 9 if a > b else 0
+print(c)
