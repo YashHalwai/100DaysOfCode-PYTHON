e7814b724984da0aca2d88b120c876d9cecf6781
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-26 16:12:38 +0530

    How import works in Python

---
+++ Day_44/harry.py
@@ -0,0 +1,7 @@
+# How import works in Python | Python Tutorial - Day #44
+
+# https://www.youtube.com/watch?v=Pr7UOr35NcI&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=44
+
+
+def welcome():
+    print("Hey you are welcome my friend")
---
+++ Day_44/import.py
@@ -0,0 +1,30 @@
+# How import works in Python | Python Tutorial - Day #44
+
+# https://www.youtube.com/watch?v=Pr7UOr35NcI&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=44
+
+
+# import pandas
+
+# pandas.read_csv()
+
+
+# import math
+
+# print(math.floor(4.2345))
+
+# result = math.sqrt(9)
+# print(result) # O/P => 3.0
+
+
+# from math import sqrt, pi
+
+# result = sqrt(9) * pi
+# print(result)
+
+
+# from math import *
+
+
+# from harry import welcome
+
+# welcome()
