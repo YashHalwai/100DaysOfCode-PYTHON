07b5c785c41bd1ad5eae40791626cff00c9b1bb3
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-01-26 15:26:03 +0530

    Virtual Environment in Python

---
+++ Day_43/virtualE.py
@@ -0,0 +1,15 @@
+# Virtual Environment in Python | Python Tutorial - Day #43
+
+# https://www.youtube.com/watch?v=nt6LlFTWOkg&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=43
+
+
+# Create a virtual environment
+# python -m venv myenv
+
+
+# Activate the virtual environment (Linux / MacOS)
+# source myenv/bin/activate
+
+
+# Activate the virtual environment (Windows)
+# myenv\Scripts\activate.bat
