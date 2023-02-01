7fad993df625765ec0ac06ec05eb5dfb6d8f8da7
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-02-01 23:57:59 +0530

    os Module in Python

---
+++ Day_46/os.py
@@ -0,0 +1,28 @@
+# os Module in Python | Python Tutorial - Day #46
+
+# https://www.youtube.com/watch?v=dkVYSsL90Oo&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=46
+
+# os.mkdir("data")
+
+import os
+
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+
+
+# for i in range(0, 100):
+#     os.rename(f"data/Tutorial{i+1}",f"data/Tutorial_{i+1}")  #mkdir
+
+
+# folders = os.listdir("data")
+
+# print(folders)
+
+# for folder in folders:
+    # print(folder)
+    # print(os.listdir(f"data/{folder}"))
+    
+    
+# print(os.getcwd())    
+
+
