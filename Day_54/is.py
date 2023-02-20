91a2e14b2d4fd8fa543a8e2418a438110b096232
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-02-20 23:19:33 +0530

    'is' vs '==' in Python

---
+++ Day_54/is.py
@@ -0,0 +1,25 @@
+# 'is' vs '==' in Python | Python Tutorial - Day #54
+
+# https://www.youtube.com/watch?v=TOemdfX_0xc&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=54
+
+
+# a = 4
+# b = "4"
+
+# print(a is b)
+# print(a == b)
+
+
+# a = [1, 2, 23]
+# b = [1, 2, 23]
+
+# print(a is b)
+# print(a == b)
+
+
+a = None
+b = None
+
+print(a is b)
+print(a is None)
+print(a == b)
