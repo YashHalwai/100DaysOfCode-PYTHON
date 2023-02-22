974ccbac85a115bac14255018e6eab0bd43f3130
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-02-22 22:37:36 +0530

    Introduction to OOPs in Python

---
+++ Day_56/IntroOOPs.py
@@ -0,0 +1,22 @@
+# Introduction to OOPs in Python | Python Tutorial - Day #56
+
+# https://www.youtube.com/watch?v=HQnoYzxOHMw&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=56
+
+
+# Object Oriented Programming
+
+def hello():
+    print("Hello")
+    
+hello()
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000
+
+sales3 = 6000
+profit2 = 2000
+ad2 = 1000
