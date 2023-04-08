4a8976b8ef78fc8eced1ea5cba34ce7f19e732c2
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-04-08 11:55:16 +0530

    dir, __dict__ and help method in Python

---
+++ Day_71/method.py
@@ -0,0 +1,22 @@
+# dir, __dict__ and help method in Python | Python Tutorial - Day #71
+
+# https://www.youtube.com/watch?v=Wgo9TaBcuJ4&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=71
+
+
+# x = [1,2,3,4,5]
+# print(dir(x))
+# print(x.__add__)
+
+
+# class Person:
+#     def ___init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#         self.version = 1
+    
+# p = Person("John", 30)
+# print(p.__dict__)        
+
+
+# print(help(str))
+
