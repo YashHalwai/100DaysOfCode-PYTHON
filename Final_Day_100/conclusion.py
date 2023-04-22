4f3cea718fb285d24395b6c6c8fe2d58b0dafa26
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-04-22 12:00:54 +0530

    Day 100 Completed

---
+++ Final_Day_100/conclusion.py
@@ -0,0 +1,19 @@
+# Conclusion and Where to go from here | Python Tutorial - Day #100
+
+# https://www.youtube.com/watch?v=5Pz8WGiMJ_c&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=100
+
+#  https://replit.com/@codewithharry/100-Day-100-Conclusion-Where-to-go-from-here#.tutorial/01-conclusion.md
+
+'''
+Where to go from here:
+To continue your learning journey, consider exploring the following resources:
+
+Python books: There are many excellent books on Python that can help you deepen your knowledge and skills. Some popular options include "Python Crash Course" by Eric Matthes, "Automate the Boring Stuff with Python" by Al Sweigart, and "Fluent Python" by Luciano Ramalho. I would also like to recommend "Hands on Machine Learning book by Aurélien Géron"
+
+YouTube Projects: There are many YouTube projects available which can be watched after you have some basic understanding of python
+
+Python communities: There are many online communities where you can connect with other Python learners and experts, ask questions, and share your knowledge. Some popular options include the Python subreddit, the Python Discord server, and the Python community on Stack Overflow.
+
+GitHub repositories: GitHub is a great resource for finding Python projects, libraries, and code snippets. Some useful repositories to check out include "awesome-python" (a curated list of Python resources), "scikit-learn" (a machine learning library), and "django" (a web development framework).
+
+'''
